c921746696ff68c18ac7e1ffd3c5c5003ea9ff2b
tithesh.karande <tirthesh.karande@easebuzz.in>
2025-01-16 10:10:15 +0530

    intial commit

---
+++ fundamentals/go.mod
@@ -0,0 +1,3 @@
+module ktirthesh/fundamentals
+
+go 1.22.0
---
+++ fundamentals/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "hello"
+	fmt.Println("a is ", a)
+
+	var b, c int = 1, 2
+	fmt.Println("b and c is ", b, c)
+
+	var d bool = true
+	fmt.Println("d is ", d)
+
+	var e bool
+	fmt.Println("e is :", e)
+
+}
