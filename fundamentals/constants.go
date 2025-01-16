3cfa31aeaa4db8339f6400dd9829c3239dc8ce0f
tithesh.karande <tirthesh.karande@easebuzz.in>
2025-01-16 10:34:48 +0530

    conatnts

--- fundamentals/constants.go
+++ fundamentals/constants.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func constants() {
-	fmt.Println("hello wotld")
+	fmt.Println("constants")
+	const n = 200330
+	const d = 3e20 / n
+	fmt.Println("n and d", n, d)
+	// fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
 }
--- fundamentals/main.go
+++ fundamentals/main.go
@@ -1,6 +1,6 @@
 package main
 
 func main() {
-	variables()
+	// variables()
 	constants()
 }
